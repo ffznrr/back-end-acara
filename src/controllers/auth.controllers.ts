4f8f955a1aa31b53e85637328e2ea824f4afdce0
ffznrr <ffznrr@gmail.com>
2025-05-21 05:57:32 +0700

    feat: api validation

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as Yup from "yup";
 
 type TRegister = {
   fullName: string;
@@ -8,9 +9,43 @@ type TRegister = {
   confirmPassword: string;
 };
 
+const registerValidateSchema = Yup.object({
+  fullName: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string().required(),
+});
+
 export default {
-  register(req: Request, res: Response) {
+  async register(req: Request, res: Response) {
     const { fullName, username, email, password, confirmPassword } =
       req.body as TRegister;
+    console.log(req.body);
+
+    try {
+      await registerValidateSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      res.status(200).json({
+        message: "succes Registration",
+        data: {
+          fullName,
+          username,
+          email,
+        },
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use("/api", router);
 
 app.listen(PORT, () => {
