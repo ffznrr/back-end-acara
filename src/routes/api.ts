edad938ecaf26ffeb0815ef844092a907760ad65
ffznrr <ffznrr@gmail.com>
2025-05-20 15:08:39 +0700

    feat: api auth register

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dummyController from "../controllers/dummy.controllers";
+import authCotnroller from "../controllers/auth.controllers";
 
 const router = express.Router();
 router.get("/dummy", dummyController.dummy);
+router.post("/auth/register", authCotnroller.register);
 
 export default router;
